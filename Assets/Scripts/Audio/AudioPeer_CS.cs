a7106f9412745d5f2bbbd3a27ec0b71821280f21
Garrett Courtney <gcourtney@student.neumont.edu>
2020-02-27 20:07:21 -0700

    Added wave manipulator for audio interaction

---
+++ Assets/Scripts/Audio/AudioPeer_CS.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioPeer_CS : MonoBehaviour
+{
+    AudioSource m_audioSource;
+    public static float[] m_samples = new float[512];
+    public static float[] m_freqBand = new float[4];
+    public static float[] m_bandBuffer = new float[4];
+    float[] m_bufferDecrease = new float[4];
+
+    float[] m_freqBandHighest = new float[4];
+    public static float[] m_audioBand = new float[4];
+    public static float[] m_audioBandBuffer = new float[4];
+
+    public static float m_amplitude;
+    public static float m_amplitudeBuffer;
+    float m_amplitudeHighest;
+
+    void Start()
+    {
+        m_audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        GetSpectrumAudioSource();
+        MakeFrequencyBands();
+        BandBuffer();
+        CreateAudioBands();
+        GetAmplitude();
+    }
+
+    void GetAmplitude()
+    {
+        float currentAmplitude = 0;
+        float currentAmplitudeBuffer = 0;
+        for (int i = 0; i < 4; i++)
+        {
+            currentAmplitude += m_audioBand[i];
+            currentAmplitudeBuffer += m_audioBandBuffer[i];
+        }
+        if (currentAmplitude > m_amplitudeHighest)
+        {
+            m_amplitudeHighest = currentAmplitude;
+        }
+        m_amplitude = currentAmplitude / m_amplitudeHighest;
+        m_amplitudeBuffer = currentAmplitudeBuffer / m_amplitudeHighest;
+
+    }
+
+    void CreateAudioBands()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            if (m_freqBand[i] > m_freqBandHighest[i])
+            {
+                m_freqBandHighest = m_freqBand;
+            }
+            m_audioBand[i] = (m_freqBand[i] / m_freqBandHighest[i]);
+            m_audioBandBuffer[i] = (m_bandBuffer[i] / m_freqBandHighest[i]);
+        }
+    }
+
+
+    void BandBuffer()
+    {
+        for (int g = 0; g < 4; g++)
+        {
+            if (m_freqBand[g] > m_bandBuffer[g])
+            {
+                m_bandBuffer[g] = m_freqBand[g];
+                m_bufferDecrease[g] = 0.005f;
+            }
+            if (m_freqBand[g] < m_bandBuffer[g])
+            {
+                m_bandBuffer[g] -= m_bufferDecrease[g];
+                m_bufferDecrease[g] *= 1.2f;
+            }
+        }
+    }
+
+    void GetSpectrumAudioSource()
+    {
+        m_audioSource.GetSpectrumData(m_samples, 0, FFTWindow.Blackman);
+    }
+
+    void MakeFrequencyBands()
+    {
+        int count = 0;
+
+        for (int i = 0; i < m_freqBand.Length; i++)
+        {
+            float average = 0;
+            int sampleCount = (int)Mathf.Pow(2, i) * 2;
+            if (i == 7)
+            {
+                sampleCount += 2;
+            }
+            for (int j = 0; j < sampleCount; j++)
+            {
+                average += m_samples[count] * (count + 1);
+                count++;
+            }
+
+            average /= count;
+            m_freqBand[i] = average * 10;
+        }
+    }
+}
---
+++ Assets/Scripts/Graphics/WaveManipulator_CS.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveManipulator_CS : MonoBehaviour
+{
+    [Header("Wave0")]
+    [SerializeField] float m_wave0FrequencyThreshhold = 0.0f;
+    public int m_bar0Band = 0;
+    bool m_bar0CycleComplete = true;
+
+    [Space]
+    [Header("Wave1")]
+    [SerializeField] float m_wave1FrequencyThreshhold = 0.0f;
+    public int m_bar1Band = 0;
+    bool m_bar1CycleComplete = true;
+
+    [Space]
+    [Header("Wave2")]
+    [SerializeField] float m_wave2FrequencyThreshhold = 0.0f;
+    public int m_bar2Band = 0;
+    bool m_bar2CycleComplete = true;
+
+    [Space]
+    [Header("Wave3")]
+    [SerializeField] float m_wave3FrequencyThreshhold = 0.0f;
+    public int m_bar3Band = 0;
+    bool m_bar3CycleComplete = true;
+
+
+    [SerializeField] [Range(0.0f, 200.0f)] float multiplier = 1.0f;
+
+    Material m_material = null;
+    float m_waveIntensity = 1.0f;
+    void Start()
+    {
+        m_material = GetComponent<Renderer>().material;
+    }
+
+    void Update()
+    {
+        UpdateDensity0();
+        UpdateDensity1();
+        UpdateDensity2();
+        UpdateDensity3();
+    }
+
+    private void OnPreRender()
+    {
+        GL.wireframe = true;
+    }
+
+    private void OnPostRender()
+    {
+        GL.wireframe = false;
+    }
+
+    void UpdateDensity0()
+    {
+        if (AudioPeer_CS.m_bandBuffer[m_bar0Band] > m_wave0FrequencyThreshhold && m_bar0CycleComplete)
+        {
+            if (m_bar0CycleComplete)
+            {
+                m_material.SetFloat("Vector1_8F67339F", m_bar0Band * multiplier);
+                m_bar0CycleComplete = false;
+            }
+        }
+        if (AudioPeer_CS.m_bandBuffer[m_bar0Band] < m_wave0FrequencyThreshhold && !m_bar0CycleComplete)
+        {
+            m_bar0CycleComplete = true;
+        }
+    }
+
+    void UpdateDensity1()
+    {
+        if (AudioPeer_CS.m_bandBuffer[m_bar1Band] > m_wave1FrequencyThreshhold && m_bar1CycleComplete)
+        {
+            if (m_bar1CycleComplete)
+            {
+                m_material.SetFloat("Vector1_BBE35185", m_bar1Band * multiplier);
+                m_bar1CycleComplete = false;
+            }
+        }
+        if (AudioPeer_CS.m_bandBuffer[m_bar1Band] < m_wave1FrequencyThreshhold && !m_bar1CycleComplete)
+        {
+            m_bar1CycleComplete = true;
+        }
+    }
+
+    void UpdateDensity2()
+    {
+        if (AudioPeer_CS.m_bandBuffer[m_bar2Band] > m_wave2FrequencyThreshhold && m_bar2CycleComplete)
+        {
+            if (m_bar2CycleComplete)
+            {
+                m_material.SetFloat("Vector1_F5C92CBB", m_bar2Band * multiplier);
+                m_bar2CycleComplete = false;
+            }
+        }
+        if (AudioPeer_CS.m_bandBuffer[m_bar2Band] < m_wave2FrequencyThreshhold && !m_bar2CycleComplete)
+        {
+            m_bar2CycleComplete = true;
+        }
+    }
+
+    void UpdateDensity3()
+    {
+        if (AudioPeer_CS.m_bandBuffer[m_bar3Band] > m_wave3FrequencyThreshhold && m_bar3CycleComplete)
+        {
+            if (m_bar3CycleComplete)
+            {
+                m_material.SetFloat("Vector1_C3489B25", m_bar3Band * multiplier);
+                m_bar3CycleComplete = false;
+            }
+        }
+        if (AudioPeer_CS.m_bandBuffer[m_bar3Band] < m_wave3FrequencyThreshhold && !m_bar3CycleComplete)
+        {
+            m_bar3CycleComplete = true;
+        }
+    }
+}
