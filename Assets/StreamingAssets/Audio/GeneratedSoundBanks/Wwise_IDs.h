a9d0dd4d7eb1c6807823bb60eb89f6e58e7c1e66
Colby Saenthavy <thedarkness435@gmail.com>
2020-03-03 20:06:59 -0700

    Wwise Stuff

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -11,10 +11,52 @@
 
 namespace AK
 {
-    namespace EVENTS
+    namespace STATES
     {
-        static const AkUniqueID TEST = 3157003241U;
-    } // namespace EVENTS
+        namespace HUG_STATE
+        {
+            static const AkUniqueID GROUP = 3386275297U;
+
+            namespace STATE
+            {
+                static const AkUniqueID FINISH = 2555741448U;
+                static const AkUniqueID INTENSE_PHASE_1 = 3052308649U;
+                static const AkUniqueID INTENSE_PHASE_2 = 3052308650U;
+                static const AkUniqueID INTENSE_PHASE_3 = 3052308651U;
+                static const AkUniqueID INTRO = 1125500713U;
+                static const AkUniqueID MED_PHASE_1 = 3201503425U;
+                static const AkUniqueID MED_PHASE_2 = 3201503426U;
+                static const AkUniqueID MED_PHASE_3 = 3201503427U;
+                static const AkUniqueID MED_PHASE_4 = 3201503428U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID REST_PHASE_1 = 1321947899U;
+                static const AkUniqueID REST_PHASE_2 = 1321947896U;
+                static const AkUniqueID REST_PHASE_3 = 1321947897U;
+                static const AkUniqueID REST_PHASE_4 = 1321947902U;
+            } // namespace STATE
+        } // namespace HUG_STATE
+
+        namespace NASTY_STATE
+        {
+            static const AkUniqueID GROUP = 1307758466U;
+
+            namespace STATE
+            {
+                static const AkUniqueID NONE = 748895195U;
+            } // namespace STATE
+        } // namespace NASTY_STATE
+
+        namespace TOWN_STATE
+        {
+            static const AkUniqueID GROUP = 4156162475U;
+
+            namespace STATE
+            {
+                static const AkUniqueID NONE = 748895195U;
+            } // namespace STATE
+        } // namespace TOWN_STATE
+
+    } // namespace STATES
 
     namespace BANKS
     {
