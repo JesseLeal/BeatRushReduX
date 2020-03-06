3f550cbed1623455e168d0c52d7a2a375d1861e0
Jesse Leal <38059219+JesseLeal@users.noreply.github.com>
2020-03-05 19:14:11 -0700

    Added MenuManager script

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] GameObject m_TitleScreen = null;
+    [SerializeField] GameObject m_OptionsScreen = null;
+    [SerializeField] GameObject m_LevelScreen = null;
+
+    [SerializeField] Toggle m_TiltToggle = null;
+    [SerializeField] Slider m_MusicSlider = null;
+    [SerializeField] Slider m_SFXSlider = null;
+    [SerializeField] AudioMixer m_Mixer = null;
+
+    void Start()
+    {
+        m_TiltToggle.isOn = PlayerPrefs.GetInt("TiltControls", 0) == 1;
+        m_MusicSlider.value = PlayerPrefs.GetInt("MusicVolume", 100);
+        m_SFXSlider.value = PlayerPrefs.GetInt("SFXVolume", 100);
+
+        ToTitle();
+    }
+    
+    void Update()
+    {
+        PlayerPrefs.SetInt("TiltControls", m_TiltToggle.isOn ? 1 : 0);
+        PlayerPrefs.SetInt("MusicVolume", Mathf.FloorToInt(m_MusicSlider.value));
+        PlayerPrefs.SetInt("SFXVolume", Mathf.FloorToInt(m_SFXSlider.value));
+
+        //also set the groups inside the audiomixer
+        //needs a bit more research first
+    }
+
+    public void ToTitle()
+    {
+        m_TitleScreen.SetActive(true);
+
+        m_OptionsScreen.SetActive(false);
+        m_LevelScreen.SetActive(false);
+    }
+
+    public void ToOptions()
+    {
+        m_OptionsScreen.SetActive(true);
+
+        m_TitleScreen.SetActive(false);
+        m_LevelScreen.SetActive(false);
+    }
+
+    public void ToLevel()
+    {
+        m_LevelScreen.SetActive(true);
+
+        m_TitleScreen.SetActive(false);
+        m_OptionsScreen.SetActive(false);
+    }
+
+    public void PlayLevel(int level)
+    {
+        SceneManager.LoadScene("Track" + level);
+    }
+}
