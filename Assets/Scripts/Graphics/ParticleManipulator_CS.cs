a5111506bc0b567555f29c3d84795f4a84c14f74
Garrett Courtney <gcourtney@student.neumont.edu>
2020-03-05 18:11:34 -0700

    Changes to wave shader; Added particles system to game world.

---
+++ Assets/Scripts/Graphics/ParticleManipulator_CS.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class ParticleManipulator_CS : MonoBehaviour
+{
+    ParticleSystem m_ps = null;
+    void Start()
+    {
+        m_ps = GetComponent<ParticleSystem>();
+    }
+
+    void Update()
+    {
+
+    }
+}
