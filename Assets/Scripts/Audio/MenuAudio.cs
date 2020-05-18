97b09835b1e0701b94bce4e88b641a4c0f2bc17c
Colby Saenthavy <thedarkness435@gmail.com>
2020-05-18 10:21:35 -0600

    FINI

---
+++ Assets/Scripts/Audio/MenuAudio.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuAudio : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    public AudioSource menuSong;
+
+    // Update is called once per frame
+    private void Start()
+    {
+        menuSong.volume = 0.0f;
+    }
+    void Update()
+    {
+        if(menuSong.volume <= 1f)
+        {
+            menuSong.volume += 0.002f;
+        }
+    }
+}
